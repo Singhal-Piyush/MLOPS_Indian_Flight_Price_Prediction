e5e5941d7b31018509e3c15bb3c7aedafbd3f998
Singhal-Piyush <singhalpiyush3610@gmail.com>
2025-04-17 11:31:02 +0530

    requirements.txt and setup.p filey

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e.'
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    This function will return the list of packages from requirements.txt
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n','') for req in requirements ]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+
+setup(
+
+name = "Indian Flight Price Prediction",
+version = "0.0.1",
+author = "Piyush Singhal",
+author_email = "singhalpiyush3610@gmail.com",
+packages = find_packages(),
+install_packages = get_requirements('requirements.txt'),
+
+)
+
